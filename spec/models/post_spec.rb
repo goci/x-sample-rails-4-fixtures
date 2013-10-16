495aca4591c966897e40e4cf869badb97f0a1ce8
Sahil Muthoo <sahil.muthoo@gmail.com>
2013-10-16 14:40:31 +0530

    unicode 2

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,11 +3,17 @@ require 'spec_helper'
 describe Post do
   let(:fixture) { File.open(File.dirname(__FILE__) + "/../fixtures/post.txt") }
   let(:fixture_unicode) { File.open(File.dirname(__FILE__) + "/../fixtures/À♛.txt") }
+  let(:fixture_unicode_2) { File.open(File.dirname(__FILE__) + "/../fixtures/banièrebetalogo3 ][ (1).txt") }
   subject { post }
 
   context "with unicode" do
     let(:post) { Post.new(title: 'post', body: File.read(fixture_unicode)) }
     its(:body) { should eq('♜Ƈ') }
+  end
+
+  context "with unicode 2" do
+    let(:post) { Post.new(title: 'post', body: File.read(fixture_unicode_2)) }
+    its(:body) { should eq('Hey.') }
   end
 
   context "with ascii" do
