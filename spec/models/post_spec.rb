d6fb3a031b9645dd6d240b25ff7efac4b2e71a3f
Akshay Karle <akshay.a.karle@gmail.com>
2013-10-16 17:20:58 +0530

    Fix it

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,10 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Post do
   let(:fixture) { File.open(File.dirname(__FILE__) + "/../fixtures/post.txt") }
   let(:fixture_unicode) { File.open(File.dirname(__FILE__) + "/../fixtures/À♛.txt") }
-  let(:fixture_unicode_2) { File.open(File.dirname(__FILE__) + "/../fixtures/banièrebetalogo3 ][ (1).txt") }
+  let(:fixture_unicode_2) { File.open(File.dirname(__FILE__) + "/../fixtures/banie\xCC\x80rebetalogo3 ][ (1).txt") }
   subject { post }
 
   context "with unicode" do
